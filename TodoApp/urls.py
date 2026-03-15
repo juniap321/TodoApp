a58f425236c32da80fa51338a4e1517dd6939151
juniap@321 <juniap320@gmail.com>
2026-03-15 20:12:48 +0530

    edit issue fix

--- TodoApp/urls.py
+++ TodoApp/urls.py
@@ -5,11 +5,13 @@ urlpatterns = [
     # HTML Page
     path('', todo_page, name="todo-page"),
     path("add/", AddTodoView.as_view(), name="add"),
-    path('update/<int:pk>/', UpdateTodoView.as_view(), name="update"),
+    path('edit/<int:pk>/', EditTodoView.as_view(), name="update"),
     path('delete/<int:pk>/', DeleteTodoView.as_view(), name="delete"),
 
 
     # REST APIs
     path('api/todos/', TodoListCreateView.as_view()),
+    path('api/tododetails/', TodoDetailView.as_view()),
+
     
     ]
